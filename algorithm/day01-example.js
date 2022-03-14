24f8baca6a421ed3bcca1ed5410fa80d97e688f5
SukbeomH <brent93.dev@gmail.com>
2022-03-14 12:47:41 +0900

    algorithm

--- algorithm/day01-example.js
+++ algorithm/day01-example.js
@@ -2,12 +2,11 @@
 //1층에 있다, 100층까지 최소횟수로 올라가는 방법은?
 let now = 1;
 let destination = 100;
+let rest = destination - now + 1;
+const answer = rest / 2;
 
-let floor = destination - now + 1;
-
-if ( floor % 2 === 0 ) {
-  const answer = floor/2
+if ( rest % 2 === 0 ) {
   console.log(answer);
 } else {
-  console.log(floor / 2) + 1;
+  console.log(rest / 2) + 1;
 }
