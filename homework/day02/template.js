d098076b75463792bf398f3b41c175f0beb6cacd
SukbeomH <brent93.dev@gmail.com>
2022-03-16 12:36:12 +0900

    hw modify

--- homework/day02/template.js
+++ homework/day02/template.js
@@ -1,12 +1,16 @@
 
 function welcomeTemplate(user) {
     const welcome =
-    `${user.email}님, 환영합니다!
-    다음 정보가 올바른지 확인해주세요
-
-    주민번호 : ${user.ssn}
-    휴대폰번호 : ${user.phone}
-    선호하는 사이트 : ${user.favsite}` 
+        `
+    <html><body>
+    <h1>${user.email}님, 환영합니다!</h1>
+    </br>다음 정보가 올바른지 확인해주세요
+    <hr />
+    </br>주민번호 : ${user.ssn}
+    </br>휴대폰번호 : ${user.phone}
+    </br>선호하는 사이트 : ${user.favsite}
+    </body></html>
+    ` 
     
     console.log(welcome);
     return welcome;
