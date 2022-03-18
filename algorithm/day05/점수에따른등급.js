a0adb71ec17b0ea450661cc71e210ef5bc2c16c1
SukbeomH <brent93.dev@gmail.com>
2022-03-18 14:43:00 +0900

    algorythm class

--- algorithm/day05/점수에따른등급.js
+++ algorithm/day05/점수에따른등급.js
@@ -7,31 +7,31 @@
 // 100점 초과나 0점 미만은 "잘못된 점수입니다"라는 문구를 띄워주세요.
 
 function grade(score) {
-    let grd = "";
-    if (score < 0 || score > 100){
-        grd = "잘못된 점수입니다";
-    }
-    else if (score >= 90 && score <100) {
-        grd = "A";
-    }
-    else if (score >= 80 && score <90) {
-        grd = "B";
-    }
-    else if (score >= 70 && score <80) {
-        grd = "C";
-    }
-    else if (score >= 60 && score <70) {
-        grd = "D";
-    }
-    else if (score < 60) {
-        grd = "F";
-    }
-    console.log(grd);
+	let grd = "";
+	if (score < 0 || score > 100) {
+		grd = "잘못된 점수입니다";
+	} else if (score >= 90) {
+		// && score <= 100 의 조건은 이미 위에서 검증되었으므로 생략 가능
+		grd = "A";
+	} else if (score >= 80) {
+		// && score < 90 의 조건은 이미 위에서 검증되었으므로 생략 가능
+		grd = "B";
+	} else if (score >= 70) {
+		// ......
+		grd = "C";
+	} else if (score >= 60) {
+		// ...
+		grd = "D";
+	} else {
+		//  .. 최후에는 전부 위에서 검증되고 남은 값이므로 else, 자연스럽게 코드가 간결해졌다
+		grd = "F";
+	}
+	console.log(grd);
 }
-grade(105)  // "잘못된 점수입니다"
-grade(-10)  // "잘못된 점수입니다"
-grade(97)   // "A"
-grade(86)   // "B"
-grade(75)   // "C"
-grade(66)   // "D"
-grade(52)   // "F"
+grade(105); // "잘못된 점수입니다"
+grade(-10); // "잘못된 점수입니다"
+grade(97); // "A"
+grade(86); // "B"
+grade(75); // "C"
+grade(66); // "D"
+grade(52); // "F"
