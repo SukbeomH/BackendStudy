e6a41f86dc031948d92f6ace7621be8abea9d288
SukbeomH <brent93.dev@gmail.com>
2022-03-23 08:53:16 +0900

    replit day07

--- Backend-js-replit/day07/ArrayFrom.js
+++ Backend-js-replit/day07/ArrayFrom.js
@@ -1,15 +1,12 @@
-// 배열의 길이를 지정하면, 배열의 짝수번째 인덱스는 2로 홀수번째는 3인 배열을 생성하는 함수를 만들어주세요. 
+// 배열의 길이를 지정하면, 배열의 짝수번째 인덱스는 2로
+// 홀수번째는 3인 배열을 생성하는 함수를 만들어주세요.
 
-
-function array(n){
-  // Array.from을 사용해주세요.
-  let arr = Array.from();
-
-  arr.forEach((num, i)=>{
-
-  })
-  
-  return arr;
+function array(n) {
+	// Array.from을 사용해주세요.
+	let arr = Array.from(Array(n), (ele, idx) =>
+		idx % 2 === 0 ? (ele = 2) : (ele = 3)
+	);
+	return arr;
 }
 
 console.log(array(10)); //[ 2, 3, 2, 3, 2, 3, 2, 3, 2, 3 ]
--- Backend-js-replit/day07/Assignment/takeRank.js
+++ Backend-js-replit/day07/Assignment/takeRank.js
@@ -1,10 +1,27 @@
 //assignment
-function takeRank(arr){
-    // 아래에 코드를 작성해주세요.  
-    let answer=Array.from();
+function takeRank(arr) {
+	// 1. 어레이를 2차원으로 변환한다 (arr.length x 2)
+	// 2. 변환된 어레이의 row에는 본래 점수 값을 넣는다
+	// 2-1. 점수는 비교하여 내림차순 정렬한다.
+	// 2-2. 등수는 1부터, 점수가 왼쪽값과 동일하면 그대로, 다르면 +1
+	// 3. 변환된 어레이의 column에는 등수를 넣는다
+	// 4. 점수와 등수 위치를 치환하거나 등수만 뽑아 반환한다
 
-    return answer;
+	let answer = Array.from(arr, (ele) => {
+		let rank = 1;
+		for (let i = 0; i <= arr.length; i++) {
+			if (ele < arr[i]) {
+				rank++;
+				console.log(rank);
+			}
+		}
+		return rank;
+	});
+
+	// answer[0][i] = answer[i][0].sort((a, b) => b - a);
+	console.log(answer);
+	return answer;
 }
-
+takeRank([87, 89, 92, 100, 76]); //[4, 3, 2, 1, 5]
 // 아래의 코드는 절대로 수정하거나 삭제하지 마세요.
 module.exports = takeRank;
