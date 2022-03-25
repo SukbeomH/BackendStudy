05c9dc3ea4d7f32990645f72fca48e0cef242fa1
SukbeomH <brent93.dev@gmail.com>
2022-03-25 10:52:35 +0900

    replit day09 scrum

--- Backend-js-replit/day09/Assignment/vote.js
+++ Backend-js-replit/day09/Assignment/vote.js
@@ -1,13 +1,13 @@
 //assignment
 function vote(str) {
-	// -----------------------------------------
+	// ------------------------------- 후보 진입
 	let result = new Map();
 	for (let s of str) {
-		result.set(s, 0);
-	}
+		result.set(s, 1);
+	} // ---------------------------- 몇표 받았는가
 	for (let s of str) {
 		result.set(s, result.get(s) + 1);
-	} // --------------------------------------
+	} // ---------------------------
 	let vote = [];
 	for (let v of result.values()) {
 		vote.push(v);
@@ -16,7 +16,7 @@ function vote(str) {
 	for (let n of result.keys()) {
 		name.push(n);
 	}
-	return name[Math.max(parseInt(vote.join())) - 1];
+	return name[Math.max(parseInt(vote.join()))];
 }
 
 // 아래의 코드는 절대로 수정하거나 삭제하지 마세요.
