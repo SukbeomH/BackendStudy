6ee538cd06b8971fc0d100eee31c80cddf57fff4
SukbeomH <brent93.dev@gmail.com>
2022-03-29 11:55:19 +0900

    daily algorythm ~day17 Done

--- algorithm/day14/행렬의덧셈.js
+++ algorithm/day14/행렬의덧셈.js
@@ -8,8 +8,8 @@
 
 function solution(arr1, arr2) {
 	let answer = [];
-	let temp = [];
 	for (let i = 0; i < arr1.length; i++) {
+		let temp = [];
 		for (let j = 0; j < arr1[i].length; j++) {
 			temp.push(arr1[i][j] + arr2[i][j]);
 		}
