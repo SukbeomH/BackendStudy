db927fd970a7c19b225f6aa6639405e1d8fa4b2e
SukbeomH <brent93.dev@gmail.com>
2022-04-12 17:43:45 +0900

    class day21

---
+++ class/21-01-login-refresh-token-cookie/src/apis/products/dto/createProduct.input.ts
@@ -0,0 +1,23 @@
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { ProductSaleslocationInput } from 'src/apis/productsSaleslocation/dto/productSaleslocation.input';
+
+@InputType()
+export class CreateProductInput {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => Int)
+  price: number;
+
+  @Field(() => String)
+  description: string;
+
+  @Field(() => ProductSaleslocationInput)
+  productSaleslocation: ProductSaleslocationInput;
+
+  @Field(() => String)
+  productCategoryId: string;
+
+  @Field(() => [String])
+  productTags: string[];
+}
